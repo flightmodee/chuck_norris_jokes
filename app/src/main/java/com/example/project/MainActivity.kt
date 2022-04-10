a1ac69bdc567c29022da3caa5071f14fc0f7fec8
Samy Abdouche <samy.abdouche@gmail.com>
2022-04-10 15:01:04 +0200

    Slightly modified MainActivity

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -4,24 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle
 import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.project.adapter.JokeAdapter
-import com.example.project.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //displaying each joke on the Logcat
         JokeList.list.forEach { it ->
             Log.d(null, it)
         }
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = JokeAdapter(JokeList.list)
     }
 }
