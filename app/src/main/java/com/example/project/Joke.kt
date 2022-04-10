1f04f84e9f0796ab5f12264780133b92a60e956c
Samy Abdouche <samy.abdouche@gmail.com>
2022-04-10 17:47:46 +0200

    Modified default values for class members

--- app/src/main/java/com/example/project/Joke.kt
+++ app/src/main/java/com/example/project/Joke.kt
@@ -3,5 +3,12 @@ package com.example.project
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Joke(val categories: List<String>, val createdAt:String, val iconUrl:String, val id:String, val updatedAt:String, val url:String, val value:String) {
+data class Joke(
+    val categories: List<String> = emptyList(),
+    val createdAt:String ="",
+    val iconUrl:String="",
+    val id:String="",
+    val updatedAt:String="",
+    val url:String="",
+    val value:String) {
 }
