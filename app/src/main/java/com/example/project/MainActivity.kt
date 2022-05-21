086a701b9d435cba02670f1cc5f3774f3e230197
Samy Abdouche <samy.abdouche@gmail.com>
2022-05-21 11:25:36 +0200

    final commit

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle
 import com.example.project.adapter.JokeAdapter
+import io.reactivex.Single
+import io.reactivex.rxkotlin.subscribeBy
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //Version 1 de l'application
         val adapterList = mutableListOf<Joke>()
         JokeList.list.forEach { joke ->
             val j = Joke(value = joke)
             adapterList.add(j)
         }
         recyclerView.adapter = JokeAdapter(adapterList)
+
+        val jokeService:JokeApiService = JokeApiServiceFactory.createService()
+        val joke: Single<Joke> = jokeService.giveMeAJoke()
+
+
     }
 }
